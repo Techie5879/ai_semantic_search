ab9a2dc2b0cf3dde46629ecad36a7eb6f89808c9
Techie5879 <aritrahtml@gmail.com>
2023-04-26 02:16:24 +0530

    setup App, added dark theme switcher

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+import { Navbar } from './components/Navbar';
+import { Paths } from './components/Paths';
+import { Footer } from './components/Footer';
+
+
+const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+  return (
+    <div className={darkTheme ? 'dark' : ''}>
+        <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
+			<Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
+			<Paths />
+			<Footer />
+        </div>
+    </div>
+  )
+}
+
+export default App
