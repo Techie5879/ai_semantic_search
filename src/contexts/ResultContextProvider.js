37f07350a9455fe5ba5e35f227ece056408cc834
Techie5879 <aritrahtml@gmail.com>
2023-04-26 19:00:20 +0530

    navbar, rcp

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -1,26 +1,53 @@
-import React, {createContext, useContext, useState} from 'React';
+import React, {createContext, useContext, useState} from 'react';
+import XMLParser from 'react-xml-parser';
+
 
 const ResultContext = createContext();
 const baseUrl = 'http://export.arxiv.org/api/query?id_list=';
 
 export const ResultContextProvider = ({ children }) => {
-    const [results, setResults] = useState(false);
+    const [results, setResults] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [searchTerm, setSearchTerm] = useState('');
 
-    // maybe pass id here
-    const getResults = async () => {
+  
+    const getResults = async (id) => {
         setIsLoading(true);
-        
-        const response = await fetch(`${baseUrl}`, {
+        console.log(`${baseUrl}${id}`)
+        const response = await fetch(`${baseUrl}${id}`, {
             method: 'GET'
-        });
+		}).then(response => response.text()).then(data => {
+			var escapedData = data.replace(/%/g, '%25'); // replace % with %25
+			var xml = new XMLParser().parseFromString(escapedData);
+			return xml
+		}).catch(err => console.log(err));
+        const filteredArray = response['children'].filter(obj => obj.name === "entry" && obj.children !== undefined);
+        const childrenArray = filteredArray.map(obj => obj.children);
+        const allowedNames = ["id", "title", "summary"];
+        const filteredArrays = childrenArray.map(childArray =>
+            childArray.filter(obj => 
+                allowedNames.includes(obj.name) ||
+                obj.attributes.type === "application/pdf"
 
-        const data = await response.json();
+            )
+        );
 
+        const remappedArr = filteredArrays.map((arr) => {
+            return arr.reduce((acc, obj) => {
+              if (obj.name === 'link') {
+                acc[obj.name] = obj.attributes.href;
+              } else {
+                acc[obj.name] = obj.value;
+              }
+              return acc;
+            }, {});
+          });
+
+		const data = await remappedArr;
+          
+
+        console.log(data)
         setResults(data);
-        // delete this later
-        console.log(results);
 
         setIsLoading(false);
     }
